e3571dec5ca91d8299e2e2841e31cdf5d3ba7ecf
info@coders.VN <CodersTokyo>
2022-02-04 18:20:58 +0700

    add body

--- Area.java
+++ Area.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Area {
+    private List<Family> families;
+
+    public Area(){
+        families = new ArrayList<Family>();
+    }
+    public List<Family> getFamily() {
+        return families;
+    }
+    public void setFamily(List<Family> family) {
+        this.families = family;
+    }
     
+    public void addFamily(Family family) {
+        this.families.add(family);
+    }
 }
