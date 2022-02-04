bc8f9a0195c52f8b0095ecee1378567a92834f9b
info@coders.VN <CodersTokyo>
2022-02-04 20:42:48 +0700

    add line 9 of file Main

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Area area = new Area();
         Scanner scanner =new Scanner(System.in);
+        System.out.print("Enter the number of family: ");
         int n = scanner.nextInt();
         for (int i = 0; i < n; i++) {
             System.out.println("-----------------------------Family-----------------------------------");
