6e5ca410955b0ddbddb0d55a4975020fa4d27692
info@coders.VN <CodersTokyo>
2022-02-04 20:34:23 +0700

    add enter information of family and member

--- Main.java
+++ Main.java
@@ -1,3 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Area area = new Area();
+        Scanner scanner =new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("-----------------------------Family-----------------------------------");
+            System.out.print("Enter number of persons in family: ");
+            int numberOfPersonInFamily = scanner.nextInt();
+            System.out.print("Enter apartment number: ");
+            int apartmentNumber = scanner.nextInt();
+            System.out.println("----------------------------Person--------------------------- ");
+            List<Person> persons = new ArrayList<>();
+            for (int j = 0; j < numberOfPersonInFamily; j++){
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                System.out.print("Enter age: ");
+                String age = scanner.nextLine();
+                System.out.print("Enter job: ");
+                String job = scanner.nextLine();
+                System.out.println("Enter identity card number: ");
+                String identityCardNumber = scanner.nextLine();
+                Person person = new Person(name, age, job, identityCardNumber);
+                persons.add(person);
+            }
+            area.addFamily(new Family(numberOfPersonInFamily, apartmentNumber, persons));
+        }
+        scanner.close();
+    }
 }
